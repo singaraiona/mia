1ca6f353c81d67add005095296e2b62cfcf3c93d
Anton <singaraiona@gmail.com>
2018-01-17 19:21:48 +0200

    tests added

---
+++ tests/mia.rs
@@ -0,0 +1,34 @@
+#![feature(test)]
+extern crate test;
+#[macro_use]
+extern crate mia;
+extern crate nom;
+//
+use nom::IResult;
+use std::io::{self, Read, Write};
+use mia::parser;
+use mia::eval;
+use mia::mia::{AST, init_builtin_symbols};
+//
+macro_rules! test {
+    ($name:tt, $lhs:expr, $rhs:expr) => {
+        #[test]
+        pub fn $name() {
+            init_builtin_symbols();
+            match parser::parse($lhs.as_bytes()) {
+                IResult::Done(_, a) => assert_eq!(format!("{}", eval::fold_list(a.as_slice()).unwrap()), $rhs),
+                IResult::Error(e)   => panic!(format!("{:?}", e)),
+                _                   => panic!("unknown error."),
+            }
+        }
+    }
+}
+//-------
+test!(atom, "1", "1");
+test!(list, "'(1 2)", "(1 2)");
+test!(vector, "#l(1 2 3)", "#l(1 2 3)");
+test!(string, "\"FOObar123\"","\"FOObar123\"");
+test!(plus,  "(+ 1 2 3)", "6");
+//test!(minus, "(- 1 2)", "-1");
+test!(setq,  "(setq t (+ 1 2)) t", "3");
+test!(eq, "(= 1 1)", "T");
